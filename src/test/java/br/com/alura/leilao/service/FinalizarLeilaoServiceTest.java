6221db3880c6b3987f31f10dbd3517c714aa2f8a
amandapazetti <amandapazettiperes@gmail.com>
2023-05-15 15:37:08 -0300

    'escrevendo o teste com mockito'

--- src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
+++ src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -1,11 +1,18 @@
 package br.com.alura.leilao.service;
 
 import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,16 +30,33 @@ class FinalizarLeilaoServiceTest {
     }
     @Test
     void deveriaFinalizarUmLeilao() {
+        List<Leilao> leiloes = leiloes();
 
-       /* Essa simulação abaixo permite que você crie um objeto que se comporte
-        de uma determinada maneira, sem precisar realmente
-         implementar todos os métodos da classe original */
-        LeilaoDao dao = Mockito.mock(LeilaoDao.class);
-
-        /* abaixo temosuma instância do objeto FinalizarLeilaoService,
+         /* abaixo temosuma instância do objeto FinalizarLeilaoService,
          passando o objeto simulado LeilaoDao como parâmetro.
           Essa é a classe que será testada e agora ela tem
           acesso ao objeto simulado LeilaoDao.*/
-        service = new FinalizarLeilaoService(dao);
+        service.finalizarLeiloesExpirados();
+    }
+
+      private List<Leilao> leiloes() {
+        List<Leilao> lista = new ArrayList<>();
+
+        Leilao leilao = new Leilao("Celular",
+                new BigDecimal("500"),
+                new Usuario("Fulano"));
+
+        Lance primeiro = new Lance(new Usuario("Beltrano"),
+                new BigDecimal("600"));
+        Lance segundo = new Lance(new Usuario("Ciclano"),
+                new BigDecimal("900"));
+
+        leilao.propoe(primeiro);
+        leilao.propoe(segundo);
+
+        lista.add(leilao);
+
+        return lista;
+
     }
 }
